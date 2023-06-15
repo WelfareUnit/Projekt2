d3c737eee60401be2f0b79e14055df565be02339
Jan Palczewski <palczewskijan13@gmail.com>
2023-06-15 18:53:12 +0200

    kod komentarze

--- Źródło.cpp
+++ Źródło.cpp
@@ -4,8 +4,8 @@
 #include "rk4.h"
 #define G 9.81
 #pragma warning(disable:4996)		//umożlliwienie uzywania scanf w Visual 2019
-double w0=10,a0=15,m=50,l=3;	//jakieś deaultowe wartości, są globalne bo tak xd;
-void rhs_fun(double t, double* tab,double* prawa)//funkcja obliczajaca prawą stronę równania różniczkowego
+double l=3, m=1;
+void rhs_fun(double t, double* tab,double* prawa) //funkcja obliczajaca prawą stronę równania różniczkowego
 {
 	prawa[0]=tab[1];
 	prawa[1]=-G/l*sin(tab[0]);
@@ -20,37 +20,34 @@ int main()
 {
 	double h = 0.001;					//Większa dokładność,przydatna szczególnie w niestandardowych zestawach danych ale moze troche przesadzilem
 	int n = 2;							//liczba zmiennych (u nas alfa i omega wiec 2)
-	double t = 0;						//czas poczatkowy to 0
-	double tk = 9;						//czas koncowy to 9s
-	double danepocz[2], danekonc[2];
+	double t = 0;						//czas poczatkowy to 0s
+	double tk = 6;						//czas koncowy to 6s
+	double a0 = 0, w0 = 0;				//zmienne na kąt i prędkość kątową początkowe
+	double danepocz[2], danekonc[2];	//tabela na kąt i prędkość kątową przed i po obliczeniach
 	FILE* f = fopen("wyniki.txt", "w");
-	printf("Podaj teraz warunki poczatkowe wahadla\n");
-	printf("Masa ciezarka[kg]: ");
+	printf("Proszę, podaj  wahadla\n");
+	printf("Masa kulki [kg]: ");
 	scanf("%lf", &m);
 	printf("\nDlugosc sznurka[m]: ");
 	scanf("%lf", &l);
-	printf("\nPodaj wychylenie początkowe:(katy) ");
+	printf("\nPodaj wychylenie początkowe:(deg) ");
 	scanf("%lf",&a0);
-	a0 *= 3.1415/180.0;		//zamiana kątów na radiany
-	printf("\nPodaj poczatkowa predkosc katowa:(katy/sek) ");
+	a0 *= 3.1415/180.0;					//zamiana kątów na radiany
+	printf("\nPodaj poczatkowa predkosc katowa:(deg/s) ");
 	scanf("%lf",&w0);
 	w0 *= 3.1415 / 180.0;
 	danepocz[0] = a0;
 	danepocz[1] = w0;
-	fprintf(f, "t\tAlfa\tomega\tenergia\n");		//naglowek pliku
+	fprintf(f, "t\tKąt\tPrędkość kątowa\tEnergia całkowita\n");		//naglowek pliku
 	while (t < tk)
 	{
 		fprintf(f, "%lf\t", t + h);
-		vrk4(t, danepocz, h, n, rhs_fun, danekonc);		//liczenie metodą Rungego-Kutty
-		fprintf(f, "%lf\t%lf\t", danekonc[0]*180.0 / 3.1415,danekonc[1]*180.0 / 3.1415);		//zapisywanie do pliku
+		vrk4(t, danepocz, h, n, rhs_fun, danekonc);												//liczenie prawej strony rownania rozniczkowego metodą Rungego-Kutty
+		fprintf(f, "%lf\t%lf\t", danekonc[0]*180.0 / 3.1415,danekonc[1]*180.0 / 3.1415);		//zapisywanie do pliku, dane kątowe przeliczone na stopnie
 		fprintf(f, "%lf\n", energia(danekonc[0], danekonc[1]));
-		for (int i = 0; i < n; i++)			// wyniki jednego kroku staja sie danymi wejsciowymi nastepnego
-		{
+		for (int i = 0; i < n; i++)																// wyniki jednego kroku calkowania staja sie danymi wejsciowymi nastepnego
 			danepocz[i] = danekonc[i];
-		}
-
 		t += h;
-		
 	}
 	fclose(f);
 	return 0;
